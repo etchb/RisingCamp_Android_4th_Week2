045705b657b0edee71731a852c9b020b0996d7ba
etchb <bhongj91@gmail.com>
2022-02-15 00:45:50 +0900

    make LoginActivity

---
+++ app/src/main/java/com/bhongj/rc_weeks2/TalkPageActivity.kt
@@ -0,0 +1,11 @@
+package com.bhongj.rc_weeks2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TalkPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_talk_page)
+    }
+}
